b163e0b2ee12bc41e68cfc810f933a29d1aa3216
JeffSun93 <jeff.sunzn@gmail.com>
2026-02-20 16:43:32 +0000

    fix a bug causing not connecting remote database

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,19 @@
+// const { Pool } = require("pg");
+
+// const ENV = process.env.NODE_ENV || 'development'
+
+// require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+
+// const db = new Pool();
+
+// if (!process.env.PGDATABASE) {
+//     throw new Error("No PGDATABASE configured")
+// } else {
+//     console.log(`Connected to ${process.env.PGDATABASE}`)
+// }
+
+// module.exports = db;
+
 const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
@@ -6,19 +22,19 @@ require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
-} else {
-  console.log(
-    `Connected to ${process.env.PGDATABASE || "Remote Production Database"}`,
-  );
 }
 
 const config = {};
-
-const db = new Pool(config);
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
+
+console.log(
+  `Connected to ${process.env.PGDATABASE || "Remote Production Database"}`,
+);
+
+const db = new Pool(config);
 
 module.exports = db;
